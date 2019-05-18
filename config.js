44c8c100197614a991d633eefb72ff7e3d6953ef
Aidan Lee <Aidan@Aidans-MacBook-Pro.local>
2019-05-18 13:50:08 -0400

    initial commit

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    user: "root",
+    pword: "root", // windows leave this blank
+    host: "localhost",
+    port: "8889", // windos users - sql port is 3306
+    database: "portfolio"
+}
+
+module.exports = config;
